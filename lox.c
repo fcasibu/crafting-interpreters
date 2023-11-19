c9acff7377bac9706c96a5e454b9af9d2425941b
Francis Eugene Casibu <projectnevz@gmail.com>
2023-11-19 15:48:57 +0800

    feat: add warning message

--- lox.c
+++ lox.c
@@ -1,9 +1,15 @@
 #include "lox.h"
 
 void error(const char *file_name, int line, int col, const char *message) {
-  report(file_name, line, col, "", message);
+  report(file_name, line, col, "", message, "Error");
 }
+
+void warning(const char *file_name, int line, int col, const char *message) {
+  report(file_name, line, col, "", message, "Warning");
+}
+
 void report(const char *file_name, int line, int col, const char *where,
-            const char *message) {
-  printf("%s:%d:%d: Error%s: %s\n", file_name, line, col, where, message);
+            const char *message, const char *message_type) {
+  printf("%s:%d:%d: %s%s: %s\n", file_name, line, col, message_type, where,
+         message);
 }
--- lox.h
+++ lox.h
@@ -7,7 +7,8 @@
 #include <string.h>
 
 void error(const char *file_name, int line, int col, const char *message);
+void warning(const char *file_name, int line, int col, const char *message);
 void report(const char *file_name, int line, int col, const char *where,
-            const char *message);
+            const char *message, const char *message_type);
 
 #endif //_LOX_H_
